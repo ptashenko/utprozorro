d6966b4ee510e71c0a80c91d7442188c4d9f64dd
Artem <aptashenko2019@gmail.com>
2022-08-02 10:15:22 +0300

    restart

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,194 @@
+<template>
+  <header class="header">
+    <ul class="header__list">
+      <li class="header__item">
+        <a href="#" class="header__link"><img class="header__image--logo" src="../assets/svg/logo.svg" alt="logo"></a>
+      </li>
+      <li class="header__item header__item--contacts">
+        <ul class="header__contacts">
+          <li class="header__item"><a href="#" class="header__link header__link--bold">38(050) 574-23-62</a></li>
+          <li class="header__item"><a href="#" class="header__link header__link--bold">client.utprozorron@gmail.com</a>
+          </li>
+          <li class="header__item header__item--margin">Графік роботи: Пн-Пт, з 9:00 до 19:00</li>
+        </ul>
+      </li>
+      <li class="header__item">
+        <img @click="mobileMenuOpen()" class="header__image--burger" src="../assets/svg/burger_icons.svg" alt="logo">
+      </li>
+    </ul>
+    <transition name="slideOn">
+      <div v-if="mobileMenu" class="mobileMenu">
+        <img @click="mobileMenuOpen()" src="../assets/svg/cross.svg" alt="Закрыть" class="mobileMenu__close">
+        <ul class="mobileMenu__menuList mobileMenu__menuList--navigation">
+          <li class="mobileMenu__menuItem">
+            <a class="mobileMenu__menuLink" href="#">Про нас</a>
+          </li>
+          <li class="mobileMenu__menuItem">
+            <a class="mobileMenu__menuLink" href="#">Послуги</a>
+          </li>
+          <li class="mobileMenu__menuItem">
+            <a class="mobileMenu__menuLink" href="#">Наші переваги</a>
+          </li>
+          <li class="mobileMenu__menuItem">
+            <a class="mobileMenu__menuLink" href="#">Контакти</a>
+          </li>
+        </ul>
+        <ul class="mobileMenu__contactsList">
+          <li class="mobileMenu__contactsItem">
+            <a href="tel:0505742362" class="mobileMenu__contactsLink">38(050) 574-23-62</a>
+          </li>
+          <li class="mobileMenu__contactsItem">
+            <a href="mailto:client.utprozorro@gmail.com"
+              class="mobileMenu__contactsLink">client.utprozorro@gmail.com</a>
+          </li>
+          <li class="mobileMenu__contactsItem">
+            <a href="#" class="mobileMenu__contactsLink">Графік роботи: Пн-Пт, з 9:00 до 19:00</a>
+          </li>
+        </ul>
+      </div>
+    </transition>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobileMenu: false,
+    }
+  },
+  methods: {
+    mobileMenuOpen() {
+      if (this.mobileMenu) {
+        this.mobileMenu = false;
+      } else {
+        this.mobileMenu = true;
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+@keyframes showOn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes slide {
+  0% {
+    right: -100%;
+  }
+  100% {
+    right: 0;
+  }
+}
+.slideOn-enter-active {
+  animation: slide .5s;
+}
+
+.slideOn-leave-active {
+  animation: slide .5s reverse;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.header {
+  padding: 5px 30px 0;
+  &__list {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__image {
+    &--burger {
+      width: 50px;
+      height: 50px;
+    }
+  }
+  &__item {
+    &--contacts {
+      display: none;
+      @media (min-width: 640px) {
+        display: block;
+      }
+    }
+    &--margin {
+      position: relative;
+      margin-top: 5px;
+      font-size: 14px;
+      &::before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: -10px;
+        width: 6px;
+        height: 6px;
+        background: #1550e7;
+        border-radius: 50%;
+        transform: translateY(-50%);
+        animation: showOn 1s linear infinite alternate;
+      }
+    }
+  }
+  &__link {
+    color: #000;
+    text-decoration: none;
+    font-size: 14px;
+    &--bold {
+      font-weight: 500;
+    }
+  }
+}
+
+.mobileMenu {
+  padding: 42px 20px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 100%;
+  box-sizing: border-box;
+  background: #000;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  &__close {
+    right: 15px;
+    top: 15px;
+    position: absolute;
+    width: 18px;
+    height: 18px;
+  }
+  &__menuList {
+    &--navigation {
+      flex-grow: 1;
+    }
+  }
+  &__menuItem {
+    &:not(:last-child) {
+      margin-bottom: 15px;
+    }
+  }
+  &__menuLink {
+    font-size: 18px;
+    text-decoration: none;
+    color: #fff;
+    font-weight: 500;
+  }
+  &__contactsLink {
+    font-size: 13px;
+    color: #fff;
+    text-decoration: none;
+  }
+}
+</style>
